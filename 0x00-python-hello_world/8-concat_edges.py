4d8086c4291f95f6dab7557ef3d9467463f10c9a
oumatoulacen <oumatoulacen@gmail.com>
2023-06-06 01:49:19 +0100

    task

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-str = "Python is an interpreted, interactive, object-oriented programming\
- language that combines remarkable power with very clear syntax"
+str = "Python is an interpreted, interactive, object-oriented programming language that combines remarkable power with very clear syntax"
 print(str[39:75], end=' ')
 print(str[107:111], end=' ')
 print(str[0:6])
