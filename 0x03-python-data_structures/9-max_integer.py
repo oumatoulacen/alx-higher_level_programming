daea926ce92d90c78a7b64f7f9aee96b1179209c
oumatoulacen <oumatoulacen@gmail.com>
2023-06-17 22:01:25 +0100

    return max num in a list

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -6,6 +6,6 @@ def max_integer(my_list=[]):
     else:
         max_value = my_list[0]
         for num in my_list:
-            if num > max_value
-            max_value = num
-            return max_value
+            if num > max_value:
+                max_value = num
+                return max_value
