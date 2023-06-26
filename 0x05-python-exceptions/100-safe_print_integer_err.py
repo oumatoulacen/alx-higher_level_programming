7c11bbafc01226aeddd5d01a94f42daf655e3992
oumatoulacen <oumatoulacen@gmail.com>
2023-06-26 12:40:00 +0100

    Safe integer print with error message

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-
 import sys
+
 
 def safe_print_integer_err(value):
     try:
@@ -9,4 +9,3 @@ def safe_print_integer_err(value):
     except Exception as e:
         print("Exception: {}".format(e), file=sys.stderr)
         return False
-
