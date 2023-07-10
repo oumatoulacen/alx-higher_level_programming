bf97c58602b6da79250493d7dbe885fe4d4447d6
oumatoulacen <oumatoulacen@gmail.com>
2023-07-10 18:38:30 +0100

    Improve Geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+'''Write a class BaseGeometry'''
+
+
+class BaseGeometry():
+    '''BaseGeometry class'''
+    def area(self):
+        '''aises an Exception'''
+        raise Exception("area() is not implemented")
