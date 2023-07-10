9c7d06182c1382db47ee052b989fb11710bf80a6
oumatoulacen <oumatoulacen@gmail.com>
2023-07-11 00:26:58 +0100

    Improve rectangle

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -13,8 +13,8 @@ class Square(Rectangle):
     '''
     def __init__(self, size):
         '''Square constractor'''
-        super().__init__(size, size)
         self.integer_validator("size", size)
+        super().__init__(size, size)
         self.__size = size
 
     def area(self):
