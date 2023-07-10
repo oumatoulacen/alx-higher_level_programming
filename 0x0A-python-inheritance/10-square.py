1e4949eda58504c1a5976c3918ca335743734ac4
oumatoulacen <oumatoulacen@gmail.com>
2023-07-10 23:25:10 +0100

    Improve rectangle

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+''' Rectangle module'''
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    '''
+    Square class which inherits from Rectangle
+    Private instance attributes:
+        -size
+    '''
+    def __init__(self, size):
+        '''Square constractor'''
+        super().__init__(size, size)
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        '''counts the area of a square'''
+        return self.__size * self.__size
