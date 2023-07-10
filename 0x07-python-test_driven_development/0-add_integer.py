37bbd38a09b92ceda103b464845bd5700017dfb2
oumatoulacen <oumatoulacen@gmail.com>
2023-07-10 15:50:30 +0100

    dir list Lookup

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 
 def add_integer(a, b=98):
-    ''' adds two numbers'''
+    '''adds two numbers
+    Raises:
+        TypeError: If either of a or b is a non-integer and non-float.'''
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
