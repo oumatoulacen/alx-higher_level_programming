a53eb8f4893dfe32cf780c0dbbfff689b3029d9e
oumatoulacen <oumatoulacen@gmail.com>
2023-07-10 22:29:33 +0100

    Improve Geometry

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+'''Write a class BaseGeometry'''
+
+
+class BaseGeometry():
+    '''BaseGeometry class'''
+    def area(self):
+        '''aises an Exception'''
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        '''validates value'''
+        if type(value) is not int:
+            raise TypeError("{:s} must be an integer".format(name))
+        elif value <= 0:
+            raise ValueError("{:s} must be greater than 0".format(name))
+
+
+'''Write a class Rectangle'''
+
+
+class Rectangle(BaseGeometry):
+    '''Rectangle class'''
+    def __init__(self, width, height):
+        self.__width = super().integer_validator("width", width)
+        self.__height = super().integer_validator("height", height)
