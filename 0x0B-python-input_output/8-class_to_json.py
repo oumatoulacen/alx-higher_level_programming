9538c45b8d472719a56d39fc004990420ce40183
oumatoulacen <oumatoulacen@gmail.com>
2023-07-11 16:59:28 +0100

    Class to JSON

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -3,6 +3,7 @@
 with simple data structure (list, dictionary, string,
 integer and boolean) for JSON serialization of an object'''
 
+
 def class_to_json(obj):
     '''returns the dictionary description with simple data structure'''
     return obj.__dict__
