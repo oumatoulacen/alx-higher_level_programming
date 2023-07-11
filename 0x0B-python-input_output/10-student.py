8ffe4765f6f78fe921125afe7a525b38d9d02da3
oumatoulacen <oumatoulacen@gmail.com>
2023-07-11 17:22:41 +0100

    Student to JSON with filter

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+'''
+Write a class Student that defines a student
+'''
+
+
+class Student:
+    '''
+    Student class that defines a student by:
+        Public instance attributes:
+            first_name
+            last_name
+            age'''
+    def __init__(self, first_name, last_name, age):
+        '''class instance constractor'''
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        '''retrieves a dictionary representation of a Student instance '''
+        if attrs is None or not isinstance(attrs, list):
+            return self.__dict__
+        return {attr: getattr(self, attr) for attr in attrs if hasattr(self, attr)}
+
