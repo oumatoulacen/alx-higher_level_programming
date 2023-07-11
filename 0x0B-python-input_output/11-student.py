87938f17435c9d6a4045160ba31d6f2c5c9bd312
oumatoulacen <oumatoulacen@gmail.com>
2023-07-11 20:05:17 +0100

    Student to disk and reload

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+'''
+Write a class Student that defines a student
+'''
+
+
+class Student:
+    '''
+    Student class that defines a student by:
+        Public instance attributes:
+            first_name
+            last_name
+            age'''
+    def __init__(self, first_name, last_name, age):
+        '''class instance constractor'''
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        '''retrieves a dictionary representation of a Student instance '''
+        if attrs is None or not isinstance(attrs, list):
+            return self.__dict__
+        return {a: getattr(self, a) for a in attrs if hasattr(self, a)}
+
+    def reload_from_json(self, json):
+        '''eplaces all attributes of the Student instance'''
+        for key, value in json.items():
+            setattr(self, key, value)
