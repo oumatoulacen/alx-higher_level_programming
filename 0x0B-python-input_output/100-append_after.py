7be03d4ed6e3ba1f5f9a7b1be475c6a80d779d8b
oumatoulacen <oumatoulacen@gmail.com>
2023-07-11 20:34:43 +0100

    Search and update

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+'''Write a function that inserts a line of text to a file,
+after each line containing a specific string '''
+
+
+import os
+'''os module'''
+
+
+def append_after(filename="", search_string="", new_string=""):
+    '''inserts a line of text to a file
+    after each line containing a specific string'''
+
+    temp_filename = filename + ".tmp"
+
+    with open(filename, 'r') as file_in, open(temp_filename, 'w') as file_out:
+        for line in file_in:
+            file_out.write(line)
+
+            if search_string in line:
+                file_out.write(new_string)
+
+    with open(temp_filename, 'r') as temp_file:
+        file_data = temp_file.read()
+    with open(filename, 'w') as file:
+        file.write(file_data)
+
+    os.remove(temp_filename)
