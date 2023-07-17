5aaf763cb17480b1831f199466eae01d86bef7db
oumatoulacen <oumatoulacen@gmail.com>
2023-07-17 18:19:24 +0100

    all

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -39,9 +39,9 @@ class Square(Rectangle):
             if len(args) >= 1:
                 self.id = args[0]
             if len(args) >= 2:
-                self.__size = args[1]
+                self.size = args[1]
             if len(args) >= 3:
-                self.__x = args[2]
+                self.x = args[2]
             if len(args) >= 4:
                 self.y = args[3]
         else:
