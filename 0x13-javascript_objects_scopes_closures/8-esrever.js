1979282871ebf51dd207a790d22fe869eaa109a0
oumatoulacen <oumatoulacen@gmail.com>
2023-09-12 23:48:42 +0100

    reverse a list

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,5 +1,9 @@
 #!/usr/bin/node
 
 exports.esrever = function (list) {
-  return list.reverse();
+  const tmp = [];
+  for (let i = list.length; i > 0; i--) {
+    tmp[list.length - i] = list[i - 1];
+  }
+  return tmp;
 };
