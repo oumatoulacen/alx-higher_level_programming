67bf43357a7d9859e7a8a4177579800094567b9f
oumatoulacen <oumatoulacen@gmail.com>
2023-09-16 15:03:22 +0100

    filter states by user input safe from injection

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+''' modules '''
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    stt = sys.argv[4]
+
+    conn = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=username,
+            passwd=password,
+            db=database,
+            charset="utf8"
+            )
+    cur = conn.cursor()
+    query = "SELECT * FROM states WHERE BINARY name=%s \
+            ORDER BY id ASC"
+    cur.execute(query, (stt,))
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
