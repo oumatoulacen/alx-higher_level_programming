bc0671113d29a61d5a88e50f9c33a064694067dd
oumatoulacen <oumatoulacen@gmail.com>
2023-09-17 20:15:45 +0100

    Cities by states

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+''' modules '''
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    conn = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=username,
+            passwd=password,
+            db=database,
+            charset="utf8"
+            )
+    cur = conn.cursor()
+    query = "SELECT cities.id, cities.name, states.name\
+            FROM cities INNER JOIN states ON cities.state_id = states.id\
+            ORDER BY cities.id ASC"
+    cur.execute(query)
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
