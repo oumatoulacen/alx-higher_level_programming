5238249a6852481cc043137593832b663c805096
oumatoulacen <oumatoulacen@gmail.com>
2023-09-28 17:58:20 +0100

    send a request

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -2,15 +2,4 @@
 # Write a Bash script that takes in a URL, sends a request to that URL
 # and displays the size of the body of the response
 
-# Check if a URL argument is provided
-if [ $# -ne 1 ]; then
-  echo "Usage: $0 <URL>"
-  exit 1
-fi
-
-# URL passed as an argument
-URL="$1"
-
-# Send a curl request to the URL and save the response body to a temporary file
-
-curl -s "$URL" | wc -c 
+curl -s $1 | wc -c
