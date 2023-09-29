0d9ea858b156971de6d51907bf93b3c8e4fe7cf5
oumatoulacen <oumatoulacen@gmail.com>
2023-09-29 16:44:49 +0100

    send request with urllib

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -11,7 +11,7 @@ try:
     with urllib.request.urlopen(url) as response:
         content = response.read()
         utf8_content = content.decode('utf-8')
-        
+
         print("Body response:")
         print("\t- type:", type(content))
         print("\t- content:", content)
