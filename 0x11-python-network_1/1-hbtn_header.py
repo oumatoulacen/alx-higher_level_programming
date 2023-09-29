8fe96261dca68202e3529affda639c5761c1d0ce
oumatoulacen <oumatoulacen@gmail.com>
2023-09-29 18:32:28 +0100

    send request with urllib

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -11,5 +11,5 @@ import sys
 if __name__ == "__main__":
     url = sys.argv[1]
     with urllib.request.urlopen(url) as response:
-        request_id = response.info().get('X-Request-Id')
+        request_id = response.headers.get('X-Request-Id')
         print(request_id)
