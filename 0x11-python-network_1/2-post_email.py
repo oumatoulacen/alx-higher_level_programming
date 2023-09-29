ee400f40427de455f5bd555082f821e6637d7b1a
oumatoulacen <oumatoulacen@gmail.com>
2023-09-29 19:06:05 +0100

    send request with urllib

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,1 +1,19 @@
 #!/usr/bin/python3
+'''
+Write a Python script that takes in a URL and an email, sends a
+POST request to the passed URL with the email as a parameter,
+and displays the body of the response (decoded in utf-8)
+'''
+
+
+import urllib.request
+import urllib.parse
+import sys
+
+if __name__ == "__main__":
+    data = {'email': sys.argv[2]}
+    data = urllib.parse.urlencode(data).encode('utf-8')
+    req = urllib.request.Request(sys.argv[1], data=data, method="POST")
+    with urllib.request.urlopen(req) as res:
+        content = res.read()
+        print(content)
