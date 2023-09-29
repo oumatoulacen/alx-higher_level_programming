747cb4185248cc69f6011e33b5b9728e7dbf14cd
oumatoulacen <oumatoulacen@gmail.com>
2023-09-29 20:51:06 +0100

    fetche a url using requests

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+'''
+Write a Python script that takes in a URL and an email address, sends
+a POST request to the passed URL with the email as a parameter, and
+finally displays the body of the response.
+'''
+
+
+import sys
+import requests
+
+req = requests.post(sys.argv[1], data=sys.argv[2])
+print(res.text)
