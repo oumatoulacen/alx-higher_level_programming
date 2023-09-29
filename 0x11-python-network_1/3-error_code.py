aea25bd60b887bfbbc741bd3f9dfd1687eaa4fc0
oumatoulacen <oumatoulacen@gmail.com>
2023-09-29 19:28:12 +0100

    send request with urllib

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -5,12 +5,13 @@ and displays the body of the response (decoded in utf-8)
 '''
 
 
-import urllib
+import urllib.request
+import urllib.error
 import sys
 
 if __name__ == "__main__":
     try:
-        with urllib.request.Request(sys.argv[1]) as res:
+        with urllib.request.urlopen(sys.argv[1]) as res:
             print(res.read().decode("utf-8"))
     except urllib.error.HTTPError as e:
         print("Error code:", e.code)
