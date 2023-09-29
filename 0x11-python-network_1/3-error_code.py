74b8b83474bc5431ff2ca71daf9e4bc907f694a4
oumatoulacen <oumatoulacen@gmail.com>
2023-09-29 19:21:29 +0100

    send request with urllib

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 '''
-Write a Python script that takes in a URL and an email, sends a
-POST request to the passed URL with the email as a parameter,
+Write a Python script that takes in a URL, sends a request to the URL
 and displays the body of the response (decoded in utf-8)
 '''
 
@@ -10,8 +9,8 @@ import urllib
 import sys
 
 if __name__ == "__main__":
-    data = { 'email': sys.argv[2],}
-    data = urllib.parse.urlencode(data).encode('utf-8')
-    with urllib.request.Request(sys.argv[1], data=data, method="POST") as res:
-        content = res.read()
-        print(content)
+    try:
+        with urllib.request.Request(sys.argv[1]) as res:
+            print(res.read().decode("utf-8"))
+    except urllib.error.HTTPError as e:
+        print("Error code:", e.code)
