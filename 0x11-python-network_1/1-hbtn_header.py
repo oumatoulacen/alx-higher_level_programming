73e720adfa6ec1be584ea08e685fa28ef53e1218
oumatoulacen <oumatoulacen@gmail.com>
2023-09-29 16:54:18 +0100

    send request with urllib

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,1 +1,19 @@
 #!/usr/bin/python3
+'''Write a Python script that takes in a URL, sends a request to the
+URL and displays the value of the X-Request-Id variable found in
+the header of the response.
+'''
+
+
+import urllib.request
+import sys
+
+url = sys.argv[1]
+
+try:
+    with urllib.request.urlopen(url) as response:
+        if 'X-Request-Id' in response.headers:
+            request_id = response.headers['X-Request-Id']
+            print(request_id)
+except urllib.error.URLError as e:
+    print("Error:", e)
