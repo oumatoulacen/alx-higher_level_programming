61c0e971af4f189f83e292dbd761bfa8ee48017d
oumatoulacen <oumatoulacen@gmail.com>
2023-09-29 22:24:56 +0100

    Search API

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 '''
-Write a Python script that takes in a letter and sends a POST
-request to http://0.0.0.0:5000/search_user with
-the letter as a parameter.
+Write a Python script that takes in a letter and sends a POSTvrequest
+to http://0.0.0.0:5000/search_user with the letter as a parameter.
 '''
 
 
@@ -10,13 +9,13 @@ import sys
 import requests
 
 if __name__ == "__main__":
-    q = sys.argv[1] ? len(sys.argv) == 2 : ""
+    q = "" if len(sys.argv) == 1 else sys.argv[1]
     req = requests.post("http://0.0.0.0:5000/search_user", data={"q": q})
     try:
         jsn = req.json()
         if jsn:
             print(f"[{jsn.get('id')}]: {jsn.get('name')}")
         else:
-            print("No result"
+            print("No result")
     except Exception:
         print("Not a valid JSON")
