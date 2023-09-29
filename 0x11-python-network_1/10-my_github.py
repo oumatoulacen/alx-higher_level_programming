0cc782dd8ee539b94bec60d592dae0dfd8fa21ed
oumatoulacen <oumatoulacen@gmail.com>
2023-09-29 22:48:03 +0100

    uses the GitHub API to display your id

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+'''
+Write a Python script that takes your GitHub credentials
+'''
+
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    url = "https://api.github.com/user"
+
+    try:
+        req = requests.get(url, auth=(sys.argv[1], sys.argv[2]))
+        json = req.json()
+        print(json.get("id"))
+    except Exception as e:
+        print("Error:", e)
