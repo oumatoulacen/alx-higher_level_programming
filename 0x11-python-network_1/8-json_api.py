069e54481b1766ea8da4171d9aaf40cec55247b5
oumatoulacen <oumatoulacen@gmail.com>
2023-09-29 21:51:29 +0100

    Search API

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,1 +1,24 @@
 #!/usr/bin/python3
+'''
+Write a Python script that takes in a letter and sends a POST
+request to http://0.0.0.0:5000/search_user with
+the letter as a parameter.
+'''
+
+
+import sys
+import requests
+
+if __name__ == "__main__":
+    q = ""
+    if sys.argv[1]:
+        q = sys.argv[1]
+    data = {"q": q}
+    req = requests.post("http://0.0.0.0:5000/search_user", data=data)
+    js = re.json()
+    if js == "":
+        print("No result")
+    else if type(js) == dict:
+        print(f"[{js.id}]: {js.name}")
+    else:
+        print("Not a valid JSON")
