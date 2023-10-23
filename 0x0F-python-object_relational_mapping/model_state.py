c7737647ea8025957a848d9f3f0141fdaed82597
oumatoulacen <oumatoulacen@gmail.com>
2023-10-23 02:02:00 +0100

    create state model

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-from sqlalchemy import Column ,Integer, String
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import declarative_base
 '''Write a python file that contains the class definition
 of a State and an instance Base'''
 
 Base = declarative_base()
+
 
 class State(Base):
     __tablename__ = 'states'
