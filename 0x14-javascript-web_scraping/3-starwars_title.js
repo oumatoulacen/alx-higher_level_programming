ab20b359a972a619a417295f5e3cce1e8570d295
oumatoulacen <oumatoulacen@gmail.com>
2023-11-01 03:22:01 +0100

    use GET with request js module to get title of films

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+/* Write a script that prints the title of a Star Wars
+movie where the episode number matches a given integer.
+
+The first argument is the movie ID
+You must use the Star wars API with the endpoint
+https://swapi-api.alx-tools.com/api/films/:id
+You must use the module request
+*/
+
+const request = require('request');
+
+request.get(`https://swapi-api.alx-tools.com/api/films/${process.argv[2]}`, (error, response, body) => {
+  if (!error && response.statusCode === 200) {
+    const data = JSON.parse(body);
+    console.log(`title: ${data.title}`);
+  }
+});
