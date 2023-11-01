c410c693fe8d03a22eb2208db36cf38b1b37b7e8
oumatoulacen <oumatoulacen@gmail.com>
2023-11-01 02:37:40 +0100

    witeFile to a file using fs module

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,18 +1,20 @@
 #!/usr/bin/node
 
 const fs = require('fs');
-const filepath = process.argv[2];
-const content = process.argv[3];
 
-if (!filepath) {
-  console.log('Please specify a path to a file');
+const filePath = process.argv[2];
+const contentToWrite = process.argv[3];
+
+if (!filePath || !contentToWrite) {
+  console.log('Please provide a file path and content to write as arguments.');
   process.exit(1);
 }
 
-fs.writeFile(filepath, content, 'utf8', (err) => {
+// Writing the content to the file in utf-8 encoding
+fs.writeFile(filePath, contentToWrite, 'utf8', (err) => {
   if (err) {
-    console.log(err);
+    console.error(err);
   } else {
-    console.log(content);
+    console.log(contentToWrite);
   }
 });
