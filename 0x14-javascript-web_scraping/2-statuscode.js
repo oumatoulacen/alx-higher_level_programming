d702498f673ad0d3860443367c2eed74508b36a8
oumatoulacen <oumatoulacen@gmail.com>
2023-11-01 02:50:12 +0100

    request a url

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+/*
+Write a script that display the status code of a GET request.
+
+The first argument is the URL to request (GET)
+The status code must be printed like this: code: <status code>
+You must use the module request
+*/
+
+const request = require("request");
+
+request.get(process.argv[2], (error, response, body) => {
+  if (!error) {
+    console.log("code: " + response.status);
+  }
+})
