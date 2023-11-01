e9cd9ee27155bc80c099f2c7da2673de5e8d2186
oumatoulacen <oumatoulacen@gmail.com>
2023-11-01 02:42:22 +0100

    witeFile to a file using fs module

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -9,15 +9,7 @@ If an error occurred during while writing, print the error object
 */
 const fs = require('fs');
 
-const filePath = process.argv[2];
-const contentToWrite = process.argv[3];
-
-if (!filePath || !contentToWrite) {
-  console.log('Please provide a file path and content to write as arguments.');
-  process.exit(1);
-}
-
-fs.writeFile(filePath, contentToWrite, 'utf8', (err) => {
+fs.writeFile(process.argv[2], process.argv[3], 'utf8', (err) => {
   if (err) {
     console.log(err);
   }
